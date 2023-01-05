1ec341289bf3e95c86a423b4fb5606599c99ecdd
Matthew Lee <matthewleemattner@gmail.com>
2023-01-05 23:54:40 +1000

    Mocked the train_cross_validator function for the test_search unittest

--- kdsearch/tests/test_unittests/test_search.py
+++ kdsearch/tests/test_unittests/test_search.py
@@ -1,4 +1,5 @@
 from kdsearch.search import search, train_cross_validator
+from unittest.mock import patch
 import numpy as np
 
 
@@ -16,8 +17,10 @@ def test_train_cross_validator():
     assert score == 0.5
 
 
-def test_search():
+@patch("kdsearch.search.train_cross_validator", return_value=0.5)
+def test_search(mock_train_cross_validator):
     """ Test the search function """
+    
     X = np.array([[1, 2], [3, 4], [5, 6], [7, 8], [9, 10], [11, 12]])
     y = np.array([1, 1, 1, 0, 0, 0])
     hyperparameter_ranges = {
@@ -27,7 +30,10 @@ def test_search():
     
     def model_func(X_train, y_train, X_test, y_test, hyperparameters):
         """ A model function that returns the accuracy score """
-        return 0.5
+        # Model function is never used in this test as its functionality is used
+        # in train_cross_validator which is mocked
+        return
     
     results = search(X, y, model_func, hyperparameter_ranges, 0.5, True, 3, 2)
     assert len(results) == 15
+    assert mock_train_cross_validator.call_count == 29
